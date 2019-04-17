bd29157c0419b02dde5bd78e1c99c29a759ff944
David whyte <kingstonwhyte87@gmail.com>
2019-04-17 18:53:03 +0100

    feat: article share
    - fix merge conflicts

--- src/components/SocialShare.jsx
+++ src/components/SocialShare.jsx
@@ -5,15 +5,13 @@ import React from 'react';
  * @class SocialShare
  * @extends { React.Component }
  */
-export default class  SocialShare extends React.Component {
-  render() {
-    const articleLink = window.location.href;
-    return(
-      <div>
-        <a href={`https://mail.google.com/mail/u/0/?view=cm&fs=1&to&su=Authors+Haven&body=`+articleLink} className="text-danger" rel="noopener noreferrer" target="_blank"><i className="fas fa-envelope" /></a>
-        <a href={`https://www.facebook.com/sharer.php?u=`+articleLink} className="text-primary" rel="noopener noreferrer" target="_blank"><i className="fab fa-facebook-square" /></a>
-        <a href={`https://twitter.com/intent/tweet?text=`+articleLink} className="text-white" rel="noopener noreferrer" target="_blank"><i className="fab fa-twitter-square text-info" /></a>
-      </div> 
-    );
-  }
+export default function SocialShare() {
+  const articleLink = window.location.href;
+  return (
+    <div>
+      <a href={`https://mail.google.com/mail/u/0/?view=cm&fs=1&to&su=Authors+Haven&body=${articleLink}`} className="text-danger" rel="noopener noreferrer" target="_blank"><i className="fas fa-envelope" /></a>
+      <a href={`https://www.facebook.com/sharer.php?u=${articleLink}`} className="text-primary" rel="noopener noreferrer" target="_blank"><i className="fab fa-facebook-square" /></a>
+      <a href={`https://twitter.com/intent/tweet?text=${articleLink}`} className="text-white" rel="noopener noreferrer" target="_blank"><i className="fab fa-twitter-square text-info" /></a>
+    </div>
+  );
 }
