92e467dfcb767441b0279f13c80704d302f41994
Valentine <valenstical@gmail.com>
2019-04-25 08:54:09 +0100

    feat(dashboard): add dashboard component
    - create presentational view for authors dashboard
    - modify unit tests for dashboard view component
    
    [Start #165485325]

--- src/test/views/Dashboard.test.js
+++ src/test/views/Dashboard.test.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { createMockStore } from '../setup';
 import Dashboard from '../../views/Dashboard';
-
-createMockStore();
 
 describe('<Dashboard>', () => {
   it('should render', () => {
     const wrapper = shallow(<Dashboard />);
-    expect(wrapper).toBeDefined();
+    expect(wrapper).toMatchSnapshot();
   });
 });
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -3,7 +3,189 @@ import React from 'react';
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div className="card-columns" data-pg-collapsed>
+        <div className="card">
+          <div className="card-body">
+            <div className="summary">
+              <h4 className="font-weight-bold">40</h4>
+              <span>Articles</span>
+              <i className="fas fa-file-alt" />
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <div className="summary">
+              <h4 className="font-weight-bold">35k</h4>
+              <span>Comments</span>
+              <i className="fas fa-comments" />
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <div className="summary">
+              <h4 className="font-weight-bold">12k</h4>
+              <span>Claps</span>
+              <i className="fas fa-sign-language" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-3 card" data-pg-collapsed>
+        <div className="table-responsive-sm">
+          <table className="table">
+            <thead>
+              <tr>
+                <th />
+                <th className="text-center">Comments</th>
+                <th className="text-center">Claps</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="align-middle">
+                  <p className="mb-n1 font-weight-bold">
+                How to write on any subject
+                    {' '}
+
+                  </p>
+                  <small>
+                Published Jul 24th, 2018
+
+                  </small>
+                  <div className="dropdown d-inline-block">
+                    <button className="btn dropdown-toggle btn-unfocused" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a className="dropdown-item" href="#">View article</a>
+                      <a className="dropdown-item" href="#">Edit</a>
+                      <a className="dropdown-item" href="#">Delete</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="text-center align-middle">45</td>
+                <td className="text-center align-middle">12</td>
+              </tr>
+              <tr>
+                <td className="align-middle">
+                  <p className="mb-n1 font-weight-bold">
+                How to write on any subject
+                    {' '}
+
+                  </p>
+                  <small>
+                Published Jul 24th, 2018
+
+                  </small>
+                  <div className="dropdown d-inline-block">
+                    <button className="btn dropdown-toggle btn-unfocused" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a className="dropdown-item" href="#">View article</a>
+                      <a className="dropdown-item" href="#">Edit</a>
+                      <a className="dropdown-item" href="#">Delete</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="text-center align-middle">45</td>
+                <td className="text-center align-middle">12</td>
+              </tr>
+              <tr>
+                <td className="align-middle">
+                  <p className="mb-n1 font-weight-bold">
+                How to write on any subject
+                    {' '}
+
+                  </p>
+                  <small>
+                Published Jul 24th, 2018
+
+                  </small>
+                  <div className="dropdown d-inline-block">
+                    <button className="btn dropdown-toggle btn-unfocused" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a className="dropdown-item" href="#">View article</a>
+                      <a className="dropdown-item" href="#">Edit</a>
+                      <a className="dropdown-item" href="#">Delete</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="text-center align-middle">45</td>
+                <td className="text-center align-middle">12</td>
+              </tr>
+              <tr>
+                <td className="align-middle">
+                  <p className="mb-n1 font-weight-bold">
+                How to write on any subject
+                    {' '}
+
+                  </p>
+                  <small>
+                Published Jul 24th, 2018
+
+                  </small>
+                  <div className="dropdown d-inline-block">
+                    <button className="btn dropdown-toggle btn-unfocused" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a className="dropdown-item" href="#">View article</a>
+                      <a className="dropdown-item" href="#">Edit</a>
+                      <a className="dropdown-item" href="#">Delete</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="text-center align-middle">45</td>
+                <td className="text-center align-middle">12</td>
+              </tr>
+              <tr>
+                <td className="align-middle">
+                  <p className="mb-n1 font-weight-bold">
+                How to write on any subject
+                    {' '}
+
+                  </p>
+                  <small>
+                Published Jul 24th, 2018
+
+                  </small>
+                  <div className="dropdown d-inline-block">
+                    <button className="btn dropdown-toggle btn-unfocused" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a className="dropdown-item" href="#">View article</a>
+                      <a className="dropdown-item" href="#">Edit</a>
+                      <a className="dropdown-item" href="#">Delete</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="text-center align-middle">45</td>
+                <td className="text-center align-middle">12</td>
+              </tr>
+              <tr>
+                <td className="align-middle">
+                  <p className="mb-n1 font-weight-bold">
+                How to write on any subject
+                    {' '}
+
+                  </p>
+                  <small>
+                Published Jul 24th, 2018
+
+                  </small>
+                  <div className="dropdown d-inline-block">
+                    <button className="btn dropdown-toggle btn-unfocused" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a className="dropdown-item" href="#">View article</a>
+                      <a className="dropdown-item" href="#">Edit</a>
+                      <a className="dropdown-item" href="#">Delete</a>
+                    </div>
+                  </div>
+                </td>
+                <td className="text-center align-middle">45</td>
+                <td className="text-center align-middle">12</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+
   );
 }
