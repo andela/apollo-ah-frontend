9447f2a962dbf4acde372c49addb8f865d5a47eb
Fejiro Gospel <38490848+fejiroofficial@users.noreply.github.com>
2019-04-25 06:15:10 +0100

    #164198147 Users can create articles (#13)
    * initial commit
    
    * Presentational component for feature
    
    * managing state with redux
    
    * feat: Users to create articles
    This is a commit for building a feature which allows users to create an
    article.
    [Delivers #164198147]
    
    * Implementing singleton principle with axios and fixing minor bug
    
    * Resolving requested changes
    
    * Fixing cloudinary crop ratio
    
    * Implementing session storage

---
+++ src/actions/createArticles.js
@@ -0,0 +1,57 @@
+// import axios from 'axios';
+import request from '../utils/request';
+import actionTypeGenerator from './typeGenerator';
+
+export const createArticleType = actionTypeGenerator('CREATE_ARTICLE');
+
+/**
+ * Action generator that is dispatched when user starts operation
+ * @returns {object} The action to dispatch
+ */
+export const createArticleLoading = () => ({
+  type: createArticleType.loading,
+});
+
+/**
+ * Action generator that is dispatched when operation is successful
+ * @param {string} message The response message
+ * @returns {object} The action to dispatch
+ */
+export const createArticleSuccess = message => ({
+  type: createArticleType.success,
+  message,
+});
+
+/**
+ * Action generator that is dispatched when operation fails
+ * @param {string} message The response message
+ * @returns {object} The action to dispatch
+ */
+export const createArticleFailure = message => ({
+  type: createArticleType.failure,
+  message,
+});
+
+/**
+ * action creator for creating an article
+ *
+ * @export
+ * @param string data - form data containing article details
+ * @returns {object}
+ */
+
+const createArticle = (props, requestData) => async (dispatch) => {
+  dispatch(createArticleLoading());
+  return request({
+    route: 'articles',
+    method: 'post',
+    payload: requestData.formData,
+  }).then((response) => {
+    dispatch(createArticleSuccess(response.message));
+    props.history.push(`/article/${response.data.data.slug}`);
+  }).catch((error) => {
+    dispatch(createArticleFailure(error.response.data.message));
+  });
+};
+
+export default createArticle;
