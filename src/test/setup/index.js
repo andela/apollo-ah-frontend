05b4e082e294da6a3e90967682e7d74c2aca6975
Alexandra Collins <44200495+Andraquin@users.noreply.github.com>
2019-04-26 06:25:41 +0100

    #164198150 Pagination component (#24)
    * feat(pagination): pagination component
    
    - write test
    - install react paginate
    - modify article action middleware to accept two parameters page and size
    - create pagination component
    - handle pagination logic
    - style pagination component
    
    [Finish #164198150]
    
    * Resolving conflicts and implementing feedback

--- src/test/setup/index.js
+++ src/test/setup/index.js
@@ -10,7 +10,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 export const mockState = {
-  articles: [],
   user: {
     token: '',
     isLoggedIn: false,
@@ -50,7 +49,11 @@ export const mockState = {
   createArticle: {
     loading: false,
     message: false,
-  }
+  },
+  articlesReducer: {
+    articles: []
+  },
+  articlesCategoryReducer: []
 };
 
 
