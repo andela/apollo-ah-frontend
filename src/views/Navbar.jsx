c838a71fdce1fe65661c41625d43bd3a8468f089
Ibrahim Omoniyi <ibrahimlekanomoniyi@gmail.com>
2019-04-26 01:18:13 +0100

    chore(navbar): add enhancement
    - implement PO's feedback on search bar

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -6,16 +6,35 @@ import { PropTypes } from 'prop-types';
  * @description - returns customized navigation bar for the app
  * @return {JSX}
  */
-const Navbar = ({ isLoggedIn, profile }) => (
-  <nav
+const Navbar = (props) => {
+  const {
+    isLoggedIn, profile, revealSearchBar, showSearch
+  } = props;
+  return (
+    <nav
       className={`navbar navbar-expand-lg fixed-top navbar-light ${isLoggedIn && 'navbar-logged'}`}
       id="navbar"
     >
-    <div className="container">
-      <Link className="navbar-brand" to="/">
+      <div className="container">
+        <div
+          className={(showSearch) ? 'nav-search-bar show-nav-search' : 'nav-search-bar'}
+        >
+          <form action="/search" method="get">
+            <i className="fas fa-search" role="button" />
+            <input type="text" placeholder="Search" />
+          </form>
+          <button
+            type="button"
+            className="btn btn-light"
+            onClick={revealSearchBar}
+          >
+            CANCEL
+          </button>
+        </div>
+        <Link className="navbar-brand" to="/">
           Authors Haven
-      </Link>
-      <button
+        </Link>
+        <button
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
@@ -25,16 +44,16 @@ const Navbar = ({ isLoggedIn, profile }) => (
           aria-label="Toggle navigation"
 
         >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div
           className="collapse navbar-collapse"
           id="navbarToggler17"
 
         >
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li className="nav-item dropdown">
-            <Link
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item dropdown">
+              <Link
                 className="nav-link dropdown-toggle"
                 to="/categories"
                 id="navbarDropdownMenuLink18"
@@ -42,158 +61,169 @@ const Navbar = ({ isLoggedIn, profile }) => (
                 aria-haspopup="true"
                 aria-expanded="false"
               >
-              Categories
-              {' '}
-            </Link>
-            <div
+                Categories
+                {' '}
+              </Link>
+              <div
                 className="dropdown-menu"
                 aria-labelledby="navbarDropdownMenuLink18"
               >
-              <Link className="dropdown-item" to="/search?categoryId=3">
+                <Link className="dropdown-item" to="/business">
                   Business
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=7">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/entertainment">
                   Entertainment
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=4">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/fashion">
                   Fashion
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=6">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/food">
                   Food
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=5">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/health">
                   Health
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=8">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/history">
                   History
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=1">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/technology">
                   Technology
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=2">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/sports">
                   Sports
-              </Link>
-              <div className="dropdown-divider" />
-              <Link className="dropdown-item" to="/search?categoryId=9">
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/others">
                   Others
+                </Link>
+              </div>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/authors">
+                Authors
+                {' '}
+                <span className="sr-only">(current)</span>
               </Link>
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/authors">
-              Authors
-              {' '}
-              <span className="sr-only">(current)</span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/user/bookmark">
-              Bookmarks
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">
-              <i className="fas fa-search" />
-              {' '}
-              Search
-            </Link>
-          </li>
-        </ul>
-        <ul className="navbar-nav mt-lg-0">
-          <li className="nav-item">
-            <Link
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/user/bookmark">
+                Bookmarks
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/search"
+                onClick={revealSearchBar}
+              >
+                <i className="fas fa-search" />
+                {' '}
+                Search
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav mt-lg-0">
+            <li className="nav-item">
+              <Link
                 className="nav-link btn-cta transition btn-brand"
                 data-pg-collapsed
                 to="/user/create-article"
               >
-              <i className="fas fa-pen transition" />
-              Write an article
-            </Link>
-          </li>
-          {
-            !isLoggedIn
-            && (
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-              Login
+                <i className="fas fa-pen transition" />
+                Write an article
               </Link>
             </li>
-            )
-          }
-        </ul>
-      </div>
-      {
-     isLoggedIn
-    && (
-      <div className="dropdown nav-profile">
-        <button
-    className="btn dropdown-toggle"
-    type="button"
-    id="dropdownMenuButton"
-    data-toggle="dropdown"
-    aria-haspopup="true"
-    aria-expanded="false"
-  >
-          <img
-        alt={profile.username}
-      className="nav-profile"
-      src={profile.image}
-    />
-        </button>
-        <div
-    className="dropdown-menu dropdown-menu-right"
-    aria-labelledby="dropdownMenuButton"
-  >
-          <Link className="dropdown-item" to="/user/create-article">
-      Write an article
-          </Link>
-          <div className="dropdown-divider" />
-          <Link className="dropdown-item" to="/search">
-      Search articles
-          </Link>
-          <div className="dropdown-divider" />
-          <Link className="dropdown-item" to="/user/dashboard">
-      My Dashboard
-          </Link>
-          <Link className="dropdown-item" to="/user/profile">
-      My Profile
-          </Link>
-          <Link className="dropdown-item" to="/user/bookmarks">
-      My Bookmarks
-          </Link>
-          <div className="dropdown-divider" />
-          <Link className="dropdown-item" to="/user/following">
-      Following
-          </Link>
-          <Link className="dropdown-item" to="/user/followers">
-      Followers
-          </Link>
-          <div className="dropdown-divider" />
-          <Link className="dropdown-item" to="/user/settings">
-      Settings
-          </Link>
-          <Link className="dropdown-item" to="/login">
-      Logout
-          </Link>
+            {
+              !isLoggedIn
+              && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              )
+            }
+          </ul>
         </div>
-      </div>
+        {
+          isLoggedIn
+          && (
+            <div className="dropdown nav-profile">
+              <button
+                className="btn dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <img
+                  alt={profile.username}
+                  className="nav-profile"
+                  src={profile.image}
+                />
+              </button>
+              <div
+                className="dropdown-menu dropdown-menu-right"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <Link className="dropdown-item" to="/user/create-article">
+                  Write an article
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/search">
+                  Search articles
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/user/dashboard">
+                  My Dashboard
+                </Link>
+                <Link className="dropdown-item" to="/user/profile">
+                  My Profile
+                </Link>
+                <Link className="dropdown-item" to="/user/bookmarks">
+                  My Bookmarks
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/user/following">
+                  Following
+                </Link>
+                <Link className="dropdown-item" to="/user/followers">
+                  Followers
+                </Link>
+                <div className="dropdown-divider" />
+                <Link className="dropdown-item" to="/user/settings">
+                  Settings
+                </Link>
+                <Link className="dropdown-item" to="/login">
+                  Logout
+                </Link>
+              </div>
+            </div>
 
-    )
-  }
-    </div>
-  </nav>
-);
+          )
+        }
+      </div>
+    </nav>
+  );
+};
 
 Navbar.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
   profile: PropTypes.object.isRequired,
+  revealSearchBar: PropTypes.func.isRequired,
+  showSearch: PropTypes.bool,
+};
+
+Navbar.defaultProps = {
+  showSearch: false,
 };
 
 export default Navbar;
