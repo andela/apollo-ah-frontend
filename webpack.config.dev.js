ea7c1e08120199e254176d4f365dfcd10105d8d2
Alexandra Collins <alexandracollins@Alexandras-MacBook-Pro.local>
2019-04-30 04:19:39 +0100

    resolve conflicts

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,6 +9,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
   filename: './index.html',
   title: 'Authors Haven',
 });
+
 const dotenvPlugin = new Dotenv();
 const envLoaderPlugin = new webpack.DefinePlugin({
   'process.env.APP_KEY': JSON.stringify(process.env.APP_KEY),
